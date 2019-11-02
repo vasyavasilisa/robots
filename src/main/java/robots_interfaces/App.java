927dcc9c52449e7458f25ed04b4771eb5f0e4882
vasilisa <vasilisademuanova2700@gmail.com>
2019-11-02 14:57:47 +0300

    add empty maven project

---
+++ src/main/java/robots_interfaces/App.java
@@ -0,0 +1,13 @@
+package robots_interfaces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
